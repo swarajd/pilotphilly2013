5aa15decc5fedd16021ba54c368e2010585ff44b
swarage <sdcoolio@gmail.com>
2013-11-09 15:18:21 -0500

    init rails proj

---
+++ pilotphilly2013/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pilotphilly2013::Application.config.secret_token = 'abd3f1183765cf59f5b9a1259771024f7fd375c2236ec19236bfe9d4ce52cae511af685a337ee414593bd54ca73e010e1afc3f2ca3f615bfaa43bb99721d01ed'
