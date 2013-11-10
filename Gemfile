aea0b81b4dc00a2fd3e38132bc4c323dd60d9c96
swarage <sdcoolio@gmail.com>
2013-11-09 21:22:16 -0500

    complete overhaul

---
+++ Gemfile
@@ -0,0 +1,21 @@
+source 'https://rubygems.org'
+ruby '1.9.3'
+gem 'rails', '3.2.12'
+gem 'sqlite3'
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+gem 'jquery-rails'
+gem 'bootstrap-sass', '>= 3.0.0.0'
+gem 'devise'
+gem 'figaro'
+gem 'simple_form'
+gem 'therubyracer', :platform=>:ruby
+group :development do
+  gem 'better_errors'
+  gem 'binding_of_caller', :platforms=>[:mri_19, :mri_20, :rbx]
+  gem 'hub', :require=>nil
+  gem 'rails_layout'
+end
--- pilotphilly2013/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Pilotphilly2013::Application.config.secret_token = '359fd49354e140a20e928b50b184db7f0cf5dc5f89530d829917c83ae88d473c6523822fa4559ce865f8207050a189340f638cef1628d30aa16536b099bbc6ba'
