00a9dab372cbca61509b15bebcf0c145c4e1f023
swarage <sdcoolio@gmail.com>
2013-11-10 10:30:32 -0500

    make things marginally prettier

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,19 @@
 Myapp::Application.routes.draw do
-  get "Submit/submit_file"
+  #get "Submit/submit_file"
 
   authenticated :user do
     root :to => 'home#index'
   end
-  #root :to => "home#index"
-  root :to => "submit#view_files"
+  root :to => "home#index"
+  #root :to => "submit#view_files"
   get 'users/:id/view' => 'Submit#view_files'
   get 'upload/create/' => 'Upload#create'
   get 'users/:id/user_files'
   get 'Users/:id/user_files'
-  get 'users/:id/Submit/submit'
-  get 'Users/:id/Submit/submit'
-  post 'users/:id/Submit/submit' => 'Submit#submit_file'
-  post 'Users/:id/Submit/submit' => 'Submit#submit_file'
+  #get 'users/:id/Submit/submit'
+  #get 'Users/:id/Submit/submit'
+  #post 'users/:id/Submit/submit' => 'Submit#submit_file'
+  #post 'Users/:id/Submit/submit' => 'Submit#submit_file'
   devise_for :users
   resources :users
 end
