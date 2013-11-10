da7f755fd167858acbeb24774c10bfe94741b05f
SharmaNikhil <flyamonkey@gmail.com>
2013-11-10 04:49:16 -0500

    time for stack overflow (legit)

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,11 @@ Myapp::Application.routes.draw do
     root :to => 'home#index'
   end
   root :to => "home#index"
+  get 'users/:id/user_files'
   get 'Users/:id/user_files'
-  get 'Users/:id/Submit/submit_file'
   get 'users/:id/Submit/submit_file'
+  get 'Users/:id/Submit/submit_file'
+  post 'users/:id/Submit/submit_file' => 'Submit#submit_file'
   post 'Users/:id/Submit/submit_file' => 'Submit#submit_file'
   devise_for :users
   resources :users
